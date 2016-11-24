ce35b1b9d062df8165b24f52802fd5db3457bb87
fokion <fokion@textensor.com>
2016-11-24 14:18:15 +0000

    Getting queries from the request and serving a html with some values
    We read the name parameter from the request and we serve it to the page.

--- src/github.com/fokion/hello/hello.go
+++ src/github.com/fokion/hello/hello.go
@@ -2,12 +2,20 @@ package main
 
 import ("fmt";"net/http";"html/template")
 // Serve html templates . Look at golang.org/pkg/html/template
+type Page struct{
+  Name string
+}
 func main() {
   //the Must will absorb errors and halt execution if it cannot parse the template
     templates := template.Must(template.ParseFiles("templates/index.html"))
     //the func  in the HandleFunc is the handler of the request on the endpoint
     http.HandleFunc("/",func(w http.ResponseWriter,r *http.Request){
-      if err:= templates.ExecuteTemplate(w,"index.html",nil); err != nil{
+      p := Page{Name:"Gopher"}
+      //if not empty then assign
+      if name:= r.FormValue("name"); name != "" {
+        p.Name = name;
+      }
+      if err:= templates.ExecuteTemplate(w,"index.html",p); err != nil{
         http.Error(w,err.Error(),http.StatusInternalServerError)
       }
     })
