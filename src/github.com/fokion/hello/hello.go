c5d228346560f8a3d5086a2a2b20381dc9c24baf
fokion <fokion@textensor.com>
2016-11-24 13:52:29 +0000

    Simple Web server that send back a hello from server

--- src/github.com/fokion/hello/hello.go
+++ src/github.com/fokion/hello/hello.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import ("fmt";"net/http")
 
 func main() {
     fmt.Printf("hello, world\n")
+    //we can run the app by go run src/github.com/fokion/hello/hello.go 
+    //we can install it by doing go install github.com/fokion/hello
+    //
+    //the func  in the HandleFunc is the handler of the request on the endpoint
+    http.HandleFunc("/",func(w http.ResponseWriter,r *http.Request){
+      fmt.Fprintf(w,"hello from server")
+    })
+    fmt.Println(http.ListenAndServe("localhost:8181",nil))
 }
