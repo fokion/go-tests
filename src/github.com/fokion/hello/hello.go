c2376c521fa99e3b4808a009f4d943df34c3dc90
fokion <fokion@textensor.com>
2016-11-24 14:04:29 +0000

    Serving a webpage using the templates
    Serving a webpage by opening the index.html template file and on request I serve the page

--- src/github.com/fokion/hello/hello.go
+++ src/github.com/fokion/hello/hello.go
@@ -1,15 +1,15 @@
 package main
 
-import ("fmt";"net/http")
-
+import ("fmt";"net/http";"html/template")
+// Serve html templates . Look at golang.org/pkg/html/template
 func main() {
-    fmt.Printf("hello, world\n")
-    //we can run the app by go run src/github.com/fokion/hello/hello.go 
-    //we can install it by doing go install github.com/fokion/hello
-    //
+  //the Must will absorb errors and halt execution if it cannot parse the template
+    templates := template.Must(template.ParseFiles("templates/index.html"))
     //the func  in the HandleFunc is the handler of the request on the endpoint
     http.HandleFunc("/",func(w http.ResponseWriter,r *http.Request){
-      fmt.Fprintf(w,"hello from server")
+      if err:= templates.ExecuteTemplate(w,"index.html",nil); err != nil{
+        http.Error(w,err.Error(),http.StatusInternalServerError)
+      }
     })
     fmt.Println(http.ListenAndServe("localhost:8181",nil))
 }
